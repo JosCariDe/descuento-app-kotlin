e5dcd28a4b248e19434c242addba176edff56546
joseLaptop <josenarvaezcariaga@gmail.com>
2025-06-22 17:32:01 -0500

    feat/components auxiliares creado

--- app/src/main/java/edu/sena/descuentosapp/views/HomeView.kt
+++ app/src/main/java/edu/sena/descuentosapp/views/HomeView.kt
@@ -1,6 +1,7 @@
 package edu.sena.descuentosapp.views
 
 import android.annotation.SuppressLint
+import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,9 +13,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import edu.sena.descuentosapp.components.MainTextField
+import edu.sena.descuentosapp.components.SpaceH
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -25,7 +33,7 @@ fun HomeView(
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text(text = "Apps DESCUENTOS") },
+                title = { Text(text = "Apps DESCUENTOS", color = Color.White) },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary
                 )
@@ -46,6 +54,11 @@ fun ContentHomeView() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("HOla Mundo")
+        var precio by remember { mutableStateOf("") }
+        var descuento by remember { mutableStateOf("") }
+
+        MainTextField(value = precio, onValueChange = {precio = it}, label = "Precio")
+        SpaceH()
+        MainTextField(value = precio, onValueChange = {precio = it}, label = "Descuento")
     }
 }
