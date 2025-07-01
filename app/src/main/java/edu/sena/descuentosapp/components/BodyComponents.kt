ce2dd826968c989c5dbee7db170d4de246ee0758
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-01 15:08:07 -0500

    feat/Alert Component creado con exito

--- app/src/main/java/edu/sena/descuentosapp/components/BodyComponents.kt
+++ app/src/main/java/edu/sena/descuentosapp/components/BodyComponents.kt
@@ -1,16 +1,20 @@
 package edu.sena.descuentosapp.components
 
+
+import androidx.compose.material3.AlertDialog
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -69,4 +73,28 @@ fun MainButton(
     ){
         Text(text)
     }
+}
+
+@Composable
+fun Alert(
+    title: String,
+    message: String,
+    confirmText: String,
+    onConfirmClick: () -> Unit,
+    onDismissClick: () -> Unit
+){
+    AlertDialog(
+        onDismissRequest = onDismissClick,
+        title = {
+            Text(text = title)
+        },
+        text = {
+            Text(text = message)
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirmClick) {
+                Text(text = confirmText)
+            }
+        }
+    )
 }
